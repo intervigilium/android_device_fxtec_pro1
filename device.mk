b8bae46aa08b26604096e936b5902a4921534784
Suresh Koleti <skoleti@codeaurora.org>
2018-12-28 14:44:41 +0530

    pro1: IMS: decouple ims-ext-common from boot jars
    -- Include ims_ext_common permission xml.
    
    Change-Id: Ide3ecae57cc1909180c8d8f323aa31620e900888
    CRs-Fixed: 2373721

--- device.mk
+++ device.mk
@@ -420,6 +420,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     ims-ext-common \
+    ims_ext_common.xml \
     telephony-ext \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
