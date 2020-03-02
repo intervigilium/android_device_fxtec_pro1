819e43e0d622dac42e57eed856a4505050a2b6c3
Ethan Chen <intervigil@gmail.com>
2020-03-01 17:47:56 -0800

    pro1: Remove unnecessary TARGET_USES_UEFI flag
    * We do not compile our own bootloader, so this flag is unnecessary.
    
    Change-Id: I9f1be5a7097b349782f5250a373b0376b1be384c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,8 +55,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
-TARGET_USES_UEFI := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := \
 	console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc1b0000 \
