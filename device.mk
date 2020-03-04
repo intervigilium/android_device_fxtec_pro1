f8e69d6978d23803257bd48b6d7d93fa17e7047f
Tom Marshall <tdm.code@gmail.com>
2020-03-04 19:21:50 +0000

    Change name of fingerprint service to be device specific
    Change-Id: I3297e86ee981d5f65a9ed6c6bcf4ac71680200ad

--- device.mk
+++ device.mk
@@ -217,7 +217,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service \
+    android.hardware.biometrics.fingerprint@2.1-service.fxtec_msm8998 \
     fingerprintd \
     FxTecPocketMode
 
